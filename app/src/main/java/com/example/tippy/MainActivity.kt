bbf7f1a69226391426270f5de30aef9bb83a09e0
g0nnz0 <bonzai_rock@hotmail.com>
2024-01-19 01:14:25 -0300

    feat(MainActivity/ui): upgrade UX by adding some emojis depending on the tip percentage

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -12,12 +12,14 @@ import kotlin.math.log
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 10
+
 class MainActivity : AppCompatActivity() {
     private lateinit var etBaseAmount: EditText
     private lateinit var seekBarTip: SeekBar
     private lateinit var tvTipPercentLabel: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +29,17 @@ class MainActivity : AppCompatActivity() {
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
 
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+        updateTipDescription(INITIAL_TIP_PERCENT)
         seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 Log.i(TAG, "On progress changed $progress")
                 tvTipPercentLabel.text = "$progress%"
                 computeTipAndTotal()
+                updateTipDescription(progress)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -57,6 +62,17 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when(tipPercent){
+            in 0..9 -> "🐭"
+            in 10..14 -> "👍🏾"
+            in 15..19 -> "🤙🏾"
+            in 16..24 -> "🫰🏾"
+            else -> "MAEAMEE!🏖️"
+        }
+        tvTipDescription.text = tipDescription
     }
 
     private fun computeTipAndTotal() {
